6f1cb3bbe189bf748594554f79ed7410d5d06889
Solomonxq <Andrijsolop3@gmail.com>
2026-05-20 17:29:19 +0300

    Scrum-39:Alembic and models

---
+++ services/watcher/alembic/env.py
@@ -0,0 +1,56 @@
+import asyncio
+from logging.config import fileConfig
+
+from alembic import context
+from sqlalchemy.ext.asyncio import create_async_engine
+
+from app.models.base import Base
+import app.models.watch
+import app.models.alert
+from app.config import settings
+
+config = context.config
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+
+def do_run_migrations(connection):
+    context.configure(
+        connection=connection,
+        target_metadata=target_metadata,
+        compare_type=True,
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+async def run_migrations_online():
+    connectable = create_async_engine(
+        settings.DB_URL,
+        poolclass=None,
+    )
+
+    async with connectable.connect() as connection:
+        await connection.run_sync(do_run_migrations)
+
+    await connectable.dispose()
+
+
+if context.is_offline_mode():
+    url = settings.DB_URL
+
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+else:
+    asyncio.run(run_migrations_online())
